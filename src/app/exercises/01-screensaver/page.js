0ef3123bda05494acee3ccf6cbd812aa881ba727
Sonia Ruiz <yune.vk@gmail.com>
2024-09-20 16:03:24 +0200

    Refactor screensaver page and add color selection feature

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react"
+import Link from "next/link"
 
-import ScreenSaver from '../../../components/ScreenSaver';
+import ScreenSaver from "../../../components/ScreenSaver"
+
+const COLORS = [
+  "aquamarine",
+  "coral",
+  "darkorange",
+  "darkseagreen",
+  "gold",
+  "lightpink",
+  "orchid",
+]
 
 function ScreenSaverExercise() {
   return (
     <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+      <h1>Choose your color</h1>
+
+      <ul>
+        {COLORS.map((color) => (
+          <li key={color}>
+            <Link href={`/exercises/01-screensaver/${color}`}>{color}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
-  );
+  )
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverExercise
