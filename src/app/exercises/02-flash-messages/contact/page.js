150b8da2536bd3a69899e2dce852305879324eb3
Sonia Ruiz <yune.vk@gmail.com>
2024-09-26 12:29:41 +0200

    ✨ Show toast when user send contact

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,28 @@
-'use client';
-import React from 'react';
+"use client"
+import React from "react"
+
+import { useRouter } from "next/navigation"
+
+import { useToast } from "../../../../components/ToastProvider"
 
 function ContactPage() {
+  const router = useRouter()
+  const { createToast } = useToast()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    createToast(
+      "Your message was received. We'll get back to you shortly!",
+      "success"
+    )
+
+    router.push("/exercises/02-flash-messages")
+  }
+
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
@@ -14,7 +32,7 @@ function ContactPage() {
         <button>Submit</button>
       </form>
     </main>
-  );
+  )
 }
 
-export default ContactPage;
+export default ContactPage
